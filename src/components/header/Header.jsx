3840fe7bb49622b4ab77a2c54876e595a351f489
Chloe Casteran <chloe.casteran@gmail.com>
2024-12-31 17:04:26 +0100

    Ajout du header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,28 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from "react";
 import './header.scss';
 
 const Header = () =>{
-    return(
-        
-    )
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <header className="header">
+            <h1 className="header__logo">KDO</h1>
+            <div className="header__burger" onClick={toggleMenu}>
+                <i className={`fa-solid fa-bars burger-icon ${menuOpen ? "hidden" : ""}`}></i>
+            </div>
+            <nav className={`header__nav ${menuOpen ? "active" : ""}`}>
+                <i className="fa-solid fa-xmark close-icon" onClick={toggleMenu}></i>
+                <ul className="header__nav__list">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="login.html" className="login">À propos</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
